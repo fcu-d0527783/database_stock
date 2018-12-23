71883bf15b7d60d652026e5058219413019203e1
tt's pig pig <920687534@qq.com>
2018-12-23 22:59:03 +0800

    add some floder

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- 新 Bootstrap 核心 CSS 文件 -->
-  <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
+  <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
   <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
   <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
   <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
@@ -16,30 +16,30 @@
 <div class="container">
   <div class="jumbotron">
     <h1>保養品庫存管理系統</h1>
-    <p>資料庫系統</p> 
+    <p>資料庫系統</p>
   </div>
   <div class="row">
     <div class="col-sm-6">
       <h3>產品</h3>
-      <p>查詢產品資料、庫存數量</p>
-      <p>改、刪除產品資料</p>
+      <a href="./product/product_search.html">查詢產品資料、庫存數量(product_search.html)<br/></a>
+      <a href="./product/product_change.html">修改、刪除產品資料(product_change.html)</a>
     </div>
     <div class="col-sm-6">
       <h3>進、出貨</h3>
-      <p>新增、查詢進出貨單</p>
-      <p>修改、刪除進出貨單</p>
+      <a href="./order/order_search.html">新增、查詢進出貨單(orderin.html)<br/></a>
+      <a href="./order/order_change.html">修改、刪除進出貨單(orderout.html)</a>
     </div>
   </div>
   <div class="row">
     <div class="col-sm-6">
-      <h3>供應商、客戶</h3>        
-      <p>查詢買、賣家資料</p>
-      <p>更改、刪除買賣家資料</p>
+      <h3>供應商、客戶</h3>
+      <a href="./company/company_search.html">查詢買、賣家資料<br/></a>
+      <a href="./company/company_change.html">更改、刪除買、賣家資料</a>
     </div>
     <div class="col-sm-6">
-      <h3>便捷查詢</h3>        
-      <p>便捷查詢即期產品</p>
-      <p></p>
+      <h3>便捷查詢</h3>
+      <a href="./search/search_deadline.html">便捷查詢即期產品(search_deadline.html)<br/></a>
+      <a href="./search/search_stcok.html">便捷查詢庫存</a>
     </div>
   </div>
 </div>
