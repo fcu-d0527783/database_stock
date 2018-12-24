6d384f07f20ab5b2cef8f5fe17db93eb3f344a09
tt's pig pig <920687534@qq.com>
2018-12-24 22:01:35 +0800

    order search ui

---
+++ order/order_search.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>進出貨</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- 新 Bootstrap 核心 CSS 文件 -->
+  <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+  <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+  <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+  <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "stock";
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $No = $_POST["SONo"];
+  $date = $_POST["Date"];
+  $client = $_POST["ClientCode"];
+  $supplier = $_POST["SupplierCode"];
+  $employee = $_POST["EmployeeCode"];
+
+  //detect button "insert" or "search"
+  if($_POST["buttonType"] == 'Insert'){
+    insert($_POST["order"]);
+  }
+  else if($_POST["buttonType"] == 'Search'){
+    serach($_POST["order"]);
+  }
+  else{
+    echo "Error";
+  }
+
+
+
+
+  function insert($type){
+    echo $type;
+  }
+
+  function search($type){
+    echo $type;
+    $sql0 = "select * from purchase_order   ";
+  }
+?>
+</div>
+
+</body>
+</html>
